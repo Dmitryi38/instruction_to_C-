ca1e86be97f86bd67a6e6ac5db7d1dfadf5f2219
Dmitry loyko <belyi1796@gmail.com>
2023-06-15 16:18:37 +0800

    commit3

--- Seminar2/task/obj/Debug/net8.0/task.AssemblyInfo.cs
+++ Seminar2/task/obj/Debug/net8.0/task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3f8b6c2d65b22dbe6ccd9248a23ba6652542e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
